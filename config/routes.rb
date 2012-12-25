66a5aac126c3b4b8d58b8927c7a774e6cde598f0
Jefferson Carley <jeff.carley@gmail.com>
2012-12-25 08:03:17 -0600

    Code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Mytechbooks::Application.routes.draw do
+
   authenticated :user do
     root :to => 'home#index'
   end
+
   root :to => "home#index"
+
   devise_for :users
+
   resources :users
 end
