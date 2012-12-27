da76f276cf979f178e72d157d37c99cff7562048
Jefferson Carley <jeff.carley@gmail.com>
2012-12-27 10:40:35 -0600

    Adds authentication and tests for home controller.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
+
+  before (:each) do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
 
   describe "GET 'index'" do
     it "should be successful" do
