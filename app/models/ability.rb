f0da9b9dbbf6131f6428c906137fd8935f03e1e2
Jefferson Carley <jeff.carley@gmail.com>
2013-01-04 07:48:33 -0600

    Adds authorization for managing books.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+
     if user.has_role? :admin
       can :manage, :all
     end
+
+    can :manage, Book, :user_id => user.id
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
