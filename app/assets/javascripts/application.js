7c2b33885ed3047bdaf4e9e89501877761670304
jcarley <jcarley@gettyimages.com>
2013-08-11 20:53:44 -0500

    Removes rvmrc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //= require foundation/foundation.forms.js
 //= require foundation/foundation.section.js
 //= require foundation/foundation.topbar.js
-//= require app/app
+//= require ./app/app
 //= require_tree ./app/services
 //= require_tree ./app/controllers
 
